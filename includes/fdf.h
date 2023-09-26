3d95b08556e409449e5ff53f5ac72a4ffe1a8162
mendes-jv <mendes.silva.jv@gmail.com>
2023-09-26 08:58:53 -0300

    fix(norm): Refactors norm erros and minor changes

--- includes/fdf.h
+++ includes/fdf.h
@@ -1,5 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jovicto2 <jovicto2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 08:51:52 by jovicto2          #+#    #+#             */
+/*   Updated: 2023/09/26 08:52:13 by jovicto2         ###   ########.org.br   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FDF_H
-#define FDF_H
+# define FDF_H
 
 # include "../libraries/mlx42/include/MLX42/MLX42.h"
 # include "../libraries/libft/includes/libft.h"
@@ -14,5 +26,9 @@
 # ifndef HEIGHT
 #  define HEIGHT 1080
 # endif //HEIGHT
+
+# ifndef PROGRAM_NAME
+#  define PROGRAM_NAME "Fil De Fer"
+# endif //PROGRAM_NAME
 
 #endif //FDF_H
--- sources/fdf.c
+++ sources/fdf.c
@@ -1,30 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jovicto2 <jovicto2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/26 08:52:31 by jovicto2          #+#    #+#             */
+/*   Updated: 2023/09/26 08:56:45 by jovicto2         ###   ########.org.br   */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/fdf.h"
 
-static void	ft_hook(void* param);
+static void	hook(void *parameter);
 
-int main(void) {
-	mlx_t *mlx;
-	mlx_image_t *image;
+int	main(void)
+{
+	mlx_t		*mlx;
+	mlx_image_t	*image;
 
 	mlx_set_setting(MLX_MAXIMIZED, true);
 	mlx_set_setting(MLX_STRETCH_IMAGE, true);
-	mlx = mlx_init(WIDTH, HEIGHT, "Static Image Noise Simulator", true);
+	mlx = mlx_init(WIDTH, HEIGHT, PROGRAM_NAME, true);
 	image = mlx_new_image(mlx, WIDTH, HEIGHT);
-	if (!mlx || !image || mlx_image_to_window(mlx, image, 0, 0) == -1) {
+	if (!mlx || !image || mlx_image_to_window(mlx, image, 0, 0) == -1)
+	{
 		if (mlx)
 			mlx_close_window(mlx);
 		ft_printf("%s", mlx_strerror(mlx_errno));
 		return (EXIT_FAILURE);
 	}
-	mlx_loop_hook(mlx, ft_hook, mlx);
+	mlx_loop_hook(mlx, hook, mlx);
 	mlx_loop(mlx);
 	mlx_terminate(mlx);
 	return (EXIT_SUCCESS);
 }
 
-void ft_hook(void* param) {
-	mlx_t *mlx = param;
-
-	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-		mlx_close_window(mlx);
+void	hook(void *parameter)
+{
+	if (mlx_is_key_down((mlx_t *)parameter, MLX_KEY_ESCAPE))
+		mlx_close_window((mlx_t *)parameter);
 }
